99811ef3583f634c1fd0bf6181b9b1f600197d42
nnathanielleinahtann <nnathanielleinahtann@gmail.com>
2024-06-18 20:32:36 +0000

    new

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path')
+
+const app = express();
+
+const port = parseInt(process.env.PORT) || process.argv[3] || 8080;
+
+app.use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/api', (req, res) => {
+  res.json({"msg": "Hello world"});
+});
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+})
