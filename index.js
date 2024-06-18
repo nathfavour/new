01bf060d7a1b7c6ce479d3194eea69c8a309b142
nnathanielleinahtann <nnathanielleinahtann@gmail.com>
2024-06-18 20:47:39 +0000

    simplify backend

--- index.js
+++ index.js
@@ -5,16 +5,41 @@ const app = express();
 
 const port = parseInt(process.env.PORT) || process.argv[3] || 8080;
 
-app.use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Your API',
+      version: '1.0.0',
+      description: 'API for managing cards',
+    },
+    servers: [
+      {
+        url: '/', // This sets the base URL for your API to the root
+      },
+    ],
+  },
+  apis: ['./index.js'], // Path to the files containing your API routes
+};
+
+const specs = swaggerJsdoc(options);
+
+app.use('/', swaggerUi.serve, swaggerUi.setup(specs));
+
+// app.use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs');
+
+// app.get('/', (req, res) => {
+//   res.render('index');
+// });
 
 app.get('/api', (req, res) => {
-  res.json({"msg": "Hello world"});
+  res.json({"msg": " ROOT"});
 });
 
 app.listen(port, () => {
