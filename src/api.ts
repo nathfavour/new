dec5773b1087d636b902196d51ded62a645869d9
nnathanielleinahtann <nnathanielleinahtann@gmail.com>
2024-06-20 01:09:43 +0000

    eureka

--- src/api.ts
+++ src/api.ts
@@ -1,22 +1,49 @@
 import { Router } from 'express';
 
+import AdminBro from 'admin-bro';
+import AdminBroExpress from '@admin-bro/express';
+import AdminBroMongoose from '@admin-bro/mongoose';
+
 import healthCheck from '@components/healthcheck/healthCheck.router';
 import user from '@components/user/user.router';
 import cards from '@components/cards/cards.router';
-// import { adminBro, adminBroRouter } from '@components/adminbro/adminbro.router';
-import adminBro from '@components/adminbro/adminbro.router';
+
+import { UserModel } from '@components/user/user.model';
+import { CardsModel } from '@components/cards/cards.model';
+
+AdminBro.registerAdapter(AdminBroMongoose);
+
+// Configure AdminBro
+const adminBroOptions = {
+  resources: [
+    {
+      resource: UserModel,
+      options: {
+        /* ... */
+      },
+    },
+    {
+      resource: CardsModel,
+      options: {
+        /* ... */
+      },
+    },
+  ],
+  rootPath: '/admin', // Set your desired root path
+  // ... other AdminBro options
+};
+
+const adminBro = new AdminBro(adminBroOptions);
+const adminBroRouter = AdminBroExpress.buildRouter(adminBro);
 
 const router: Router = Router();
 const cors = require('cors');
 
 router.use(cors());
 
-router.use(adminBro.options.rootPath, router);
-// router.use('/admin', adminBroRouter);
-
+router.use(adminBro.options.rootPath, adminBroRouter);
 router.use(healthCheck);
 router.use(user);
 router.use(cards);
-// router.use(adminBro.options.rootPath, adminBroRouter);
 
 export default router;
