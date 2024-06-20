fd0098bfb842f3e91f855379eceb58a1e5a6ff67
nnathanielleinahtann <nnathanielleinahtann@gmail.com>
2024-06-20 00:08:20 +0000

    okay

--- src/api.ts
+++ src/api.ts
@@ -4,13 +4,16 @@ import healthCheck from '@components/healthcheck/healthCheck.router';
 import user from '@components/user/user.router';
 import cards from '@components/cards/cards.router';
 // import { adminBro, adminBroRouter } from '@components/adminbro/adminbro.router';
-import adminBroRouter from '@components/adminbro/adminbro.router';
+import adminBro from '@components/adminbro/adminbro.router';
 
 const router: Router = Router();
 const cors = require('cors');
 
-router.use('/admin', adminBroRouter);
 router.use(cors());
+
+router.use(adminBro.options.rootPath, router);
+// router.use('/admin', adminBroRouter);
+
 router.use(healthCheck);
 router.use(user);
 router.use(cards);
