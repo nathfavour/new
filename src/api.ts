cc1d35a11e8e3f20d9b7078549df6fa33c424e5b
nathfavour <116535483+nathfavour@users.noreply.github.com>
2024-07-15 16:02:51 +0100

    from locals

--- src/api.ts
+++ src/api.ts
@@ -4,13 +4,13 @@ import healthCheck from '@components/healthcheck/healthCheck.router';
 import user from '@components/user/user.router';
 import cards from '@components/cards/cards.router';
 // import { adminBro, adminBroRouter } from '@components/adminbro/adminbro.router';
-import adminBroRouter from '@components/adminbro/adminbro.router';
+// import adminBroRouter from '@components/adminbro/adminbro.router';
 
 const router: Router = Router();
 router.use(healthCheck);
 router.use(user);
 router.use(cards);
 // router.use(adminBro.options.rootPath, adminBroRouter);
-router.use('/admin', adminBroRouter);
+// router.use('/admin', adminBroRouter);
 
 export default router;
